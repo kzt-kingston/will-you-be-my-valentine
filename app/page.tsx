c26aeb8fe7e24841a9adf4fd797e890747b23d7e
kingston <kyawzinthet.developer@gmail.com>
2025-02-04 13:18:24 +0700

    update: enhance UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import { useState } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function ValentinePage() {
-  const [noCount, setNoCount] = useState(0);
-  const [yesPressed, setYesPressed] = useState(false);
-  const [yesButtonSize, setYesButtonSize] = useState(1);
+  const [noClickCount, setNoClickCount] = useState(0);
+  const [isYesPressed, setIsYesPressed] = useState(false);
+  const [yesButtonScale, setYesButtonScale] = useState(1.5);
+  const [noButtonScale, setNoButtonScale] = useState(1.5);
 
   const handleNoClick = () => {
-    setNoCount(noCount + 1);
-    setYesButtonSize(yesButtonSize + 1.2);
+    setNoClickCount(noClickCount + 1);
+    setYesButtonScale(yesButtonScale + 1);
+    setNoButtonScale(noButtonScale - 0.1);
   };
 
   const getNoButtonText = () => {
@@ -26,56 +28,74 @@ export default function ValentinePage() {
       "Think again!",
       "Last chance!",
     ];
-    return phrases[Math.min(noCount, phrases.length - 1)];
+    return phrases[Math.min(noClickCount, phrases.length - 1)];
   };
+
+  const renderYesContent = () => (
+    <div className="mt-8 text-center">
+      <Player
+        src="/lottie/v-day-yes.json"
+        loop
+        autoplay
+        style={{ height: "300px", width: "300px" }}
+      />
+      <h2 className="text-2xl font-bold text-pink-600 mb-4">
+        Yay! Happy Valentine&apos;s Day! ❤️
+      </h2>
+      <p className="text-xl text-gray-700">
+        I&apos;m so happy you said yes! 😊
+      </p>
+    </div>
+  );
+
+  const renderNoContent = () => (
+    <div>
+      <Player
+        src="/lottie/v-day.json"
+        loop
+        autoplay
+        style={{ height: "300px", width: "300px" }}
+      />
+      <h1 className="text-2xl text-center font-bold mb-8 text-pink-600">
+        Will you be my Valentine?
+      </h1>
+      <div className="flex justify-center space-x-4 overflow-hidden w-full">
+        <button
+          className="bg-green-500 hover:bg-green-600 text-white font-bold p-2 rounded-md transition-all duration-300 ease-in-out transform hover:scale-110"
+          style={{ fontSize: `${yesButtonScale}rem` }}
+          onClick={() => setIsYesPressed(true)}
+        >
+          Yes
+        </button>
+        {noClickCount < 10 && (
+          <button
+            className="bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded-md transition-all duration-300 ease-in-out transform hover:scale-110"
+            style={{ fontSize: `${noButtonScale}rem` }}
+            onClick={handleNoClick}
+          >
+            {getNoButtonText()}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100 p-5 w-full overflow-hidden">
-      {yesPressed ? (
-        <div className="mt-8 text-center">
-          <Player
-            src="/lottie/v-day-yes.json"
-            loop
-            autoplay
-            style={{ height: "300px", width: "300px" }}
-          />
-          <h2 className="text-2xl font-bold text-pink-600 mb-4">
-            Yay! Happy Valentine&apos;s Day! ❤️
-          </h2>
-          <p className="text-xl text-gray-700">
-            I&apos;m so happy you said yes! 😊
-          </p>
-        </div>
-      ) : (
-        <div>
-          <Player
-            src="/lottie/v-day.json"
-            loop
-            autoplay
-            style={{ height: "300px", width: "300px" }}
-          />
-          <h1 className="text-2xl text-center font-bold mb-8 text-pink-600">
-            Will you be my Valentine?
-          </h1>
-          <div className="flex justify-center space-x-4 overflow-hidden w-full">
-            <button
-              className={`bg-green-500 hover:bg-green-600 text-white font-bold p-2 rounded-md transition-all duration-300 ease-in-out transform hover:scale-110`}
-              style={{ fontSize: `${yesButtonSize}rem` }}
-              onClick={() => setYesPressed(true)}
-            >
-              Yes
-            </button>
-            {noCount < 10 && (
-              <button
-                className="bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded-md transition-all duration-300 ease-in-out transform hover:scale-110"
-                onClick={handleNoClick}
-              >
-                {getNoButtonText()}
-              </button>
-            )}
-          </div>
-        </div>
-      )}
+      <a
+        href="https://github.com/kzt-kingston/will-you-be-my-valentine" // Add your GitHub repo link here
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute top-4 right-4"
+      >
+        <Player
+          src="/lottie/gh-icon.json"
+          loop
+          autoplay
+          style={{ height: "50px", width: "50px" }}
+        />
+      </a>
+      {isYesPressed ? renderYesContent() : renderNoContent()}
     </div>
   );
 }
